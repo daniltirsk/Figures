1c1a6cf4498a30b29d9b6d6dc8cb14c68ddac65e
daniltirsk <56964428+daniltirsk@users.noreply.github.com>
2020-12-04 17:22:44 +0800

    Update Rectangle.h

--- Rectangle.h
+++ Rectangle.h
@@ -18,11 +18,11 @@ public:
 
     Rectangle(double w, double h);
 
-    virtual double getArea();
+    double getArea();
 
-    virtual double getPerimeter();
+    double getPerimeter();
 
-    virtual string returnType();
+    string returnType();
 };
 
 #endif
